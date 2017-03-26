1a2130832563edaa4cc0a0b1d6af5357b82d7f37
Stephen Leach <sfkleach@gmail.com>
2017-03-26 22:11:31 +0100

    Renaming file to something less commonplace to avoid name clashes and ambiguity.

--- test_lexer.py
+++ test_lexer.py
@@ -1,24 +1,30 @@
 import io
-from lexer import TokenRules, MoveSet, Accept, Erase, Pushback
+from tokeniserfactory import TokeniserFactory, MoveSet, Accept, Erase, Pushback
 
 def test_lexer():
 	ttt = (
-		TokenRules(
-		    "start",
-		    { "start": 
-		    	MoveSet(
-		            Accept( 'a', 'grab_a' ),
-		            Erase( True, 'start' ),
-		            Erase( None, lambda x: ( 'id', ''.join( x ) ) )
-		        ),
-		    "grab_a": 
-			    MoveSet(
-			        Accept( 'a', 'grab_a' ),
-			        Erase( True, lambda x: ''.join( x ) ),
-			        Pushback( None, lambda x: ( 'id', ''.join( x ) ) )
-			    )
-		    }
+		TokeniserFactory(
+		    { 
+			    "start": 
+			    	MoveSet(
+			            Accept( 'a', 'grab_a' ),
+			            Erase( True, 'start' ),
+			            Erase( None, lambda t: None )
+			        ),
+			    "grab_a": 
+				    MoveSet(
+				        Accept( 'a', 'grab_a' ),
+				        Erase( True, lambda t: t.collected() ),
+				        Pushback( None, lambda t: t.collected() )
+				    )
+		    },
+		    start="start"
 		)
 	)
+	# t = ttt( io.StringIO( 'xxxxaaabaaca' ) )
+	# print( t.nextOptToken() )
+	# print( t.nextOptToken() )
+	# print( t.nextOptToken() )
+	# print( t.nextOptToken() )
 	assert [ *ttt( io.StringIO( 'aaabaaca' ) ) ] == [ 'aaa', 'aa', 'a' ]
 
